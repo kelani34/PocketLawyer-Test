e76dc50278c625a37f03b0f1b9bbfdbda17f731b
kelani34 <kelanitaiwo34@gmail.com>
2023-02-17 09:43:06 +0100

    update

--- part4/src/components/Chat.jsx
+++ part4/src/components/Chat.jsx
@@ -1,32 +1,12 @@
 import React from "react";
-import { useEffect } from "react";
 import { useState } from "react";
-import io from "socket.io-client";
 import "./Chat.css";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
-  const [socket, setSocket] = useState(null);
-
-  useEffect(() => {
-    const newSocket = io("http://localhost:5000");
-    setSocket(newSocket);
-
-    return () => newSocket.close();
-  }, []);
-
-  useEffect(() => {
-    if (socket) {
-      socket.on("message", (message) => {
-        setMessages([...messages, message]);
-      });
-    }
-  }, [socket, messages]);
-
   function handleSubmit(event) {
     event.preventDefault();
     const newMessage = event.target.message.value;
-    socket.emit("message", newMessage);
     setMessages([...messages, newMessage]);
     event.target.reset();
   }
